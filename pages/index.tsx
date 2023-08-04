61ff68a7c77c4af75267fdd1e088fff569881468
Danish <danishqes0786@gmail.com>
2023-08-04 22:47:56 +0530

    Modified Input

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Header label="Home" />
-      <Form placeholder="What's happening ?" />
+      <Form placeholder="What's happening....?" />
       <PostFeed />
     </>
   );
